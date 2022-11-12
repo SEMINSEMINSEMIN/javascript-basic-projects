7477882e4f98fbb7672eeb7fff9c7def48eb7241
SeminKang <ksm9802@gmail.com>
2022-11-12 14:48:28 +0900

    클릭시 스팬 태그 내 텍스트 콘텐츠가 헥스코드로 바뀌도록

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,7 +1,9 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const $body = document.querySelector('body');
-const $btn = document.querySelector("#btn");
+const $cont = document.querySelector('.container');
+const $colorSpan = $cont.querySelector('.color');
+const $btn = $cont.querySelector("#btn");
 
 $btn.addEventListener('click', e => {
     const red = 
@@ -18,6 +20,7 @@ $btn.addEventListener('click', e => {
 
     const hexCode = '#' + red + green + blue;
     console.log(hexCode);
+    $colorSpan.textContent = hexCode;
     $body.style.background = hexCode;
 })
 
