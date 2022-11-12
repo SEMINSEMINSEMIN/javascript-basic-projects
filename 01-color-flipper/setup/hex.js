51238df1d7ef843f1aa512f44a25ccd73116c856
SeminKang <ksm9802@gmail.com>
2022-11-12 14:39:26 +0900

    버튼 클릭시 이벤트 달아줌 + 헥스코드 계산

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const $btn = document.querySelector("#btn");
+
+$btn.addEventListener('click', e => {
+    const red = 
+    hex[Math.floor(Math.random() * hex.length)].toString() +
+    hex[Math.floor(Math.random() * hex.length)].toString();
+
+    const green = 
+        hex[Math.floor(Math.random() * hex.length)].toString() +
+        hex[Math.floor(Math.random() * hex.length)].toString();
+
+    const blue = 
+        hex[Math.floor(Math.random() * hex.length)].toString() +
+        hex[Math.floor(Math.random() * hex.length)].toString();
+
+    const hexCode = red + green + blue;
+    console.log(hexCode);
+})
+
