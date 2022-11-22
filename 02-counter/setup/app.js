7af0764b70c7091695dd5704a4d60474d081d223
SeminKang <ksm9802@gmail.com>
2022-11-22 17:33:07 +0900

    02/dom 초기값

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,4 @@
+const countNum = document.querySelector('#value');
+const increaseBtn = document.querySelector('.btn.increase');
+const decreaseBtn = document.querySelector('.btn.decrease');
+const resetBtn = document.querySelector('.btn.reset');
