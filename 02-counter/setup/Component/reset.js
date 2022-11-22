a582065e52311eece326eac278b5825ec850f2dd
SeminKang <ksm9802@gmail.com>
2022-11-22 17:40:51 +0900

    02/reset func

---
+++ 02-counter/setup/Component/reset.js
@@ -0,0 +1,5 @@
+function reset(target){
+    target.textContent = '0';
+}
+
+export default reset;
--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,5 +1,6 @@
 import increase from "./Component/increase.js"
 import decrease from "./Component/decrease.js"
+import reset from "./Component/reset.js";
 
 const countNum = document.querySelector('#value');
 const increaseBtn = document.querySelector('.btn.increase');
@@ -8,3 +9,4 @@ const resetBtn = document.querySelector('.btn.reset');
 
 increaseBtn.addEventListener('click', e => increase(countNum));
 decreaseBtn.addEventListener('click', e => decrease(countNum));
+resetBtn.addEventListener('click', e => reset(countNum));
